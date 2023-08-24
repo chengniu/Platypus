048eb90249a79b37b0aa1cde282918b58da4b5eb
dev <juntong.song@newsbreak.com>
2023-08-24 18:11:26 +0000

    add merge.sh for one experiment

---
+++ experiments/platypus_bay_area_news/merge_platypus_bay_area_news.sh
@@ -0,0 +1,13 @@
+set -x
+set -e
+
+export PYTHONPATH=$PYTHONPATH:/home/dayong/chengniu/Platypus
+
+cd /home/dayong/chengniu/Platypus
+
+python merge.py \
+--base_model_name_or_path /home/dayong/chengniu/model/llama2_7B_hf \
+--peft_model_path /home/dayong/chengniu/model/platypus_bay_area_news \
+--output_dir /home/dayong/chengniu/model/platypus_bay_area_news_merged \
+
+cd /home/dayong/chengniu/Platypus/experiments/platypus_bay_area_news
