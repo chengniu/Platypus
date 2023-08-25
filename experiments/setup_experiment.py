09eb334c6a202b4d94597a79a7307f53148e1772
dev <juntong.song@newsbreak.com>
2023-08-25 20:57:15 +0000

    update

--- experiments/setup_experiment.py
+++ experiments/setup_experiment.py
@@ -1,20 +1,24 @@
 from experiments.experiment_util import get_program_config
 import os
 import sys
+from pathlib import Path
 
 
-def setup_experiments(env_name, fine_tune_name, platypus_src_path, lm_evaluation_src_path):
+def setup_experiments(env_name, fine_tune_name, qa_test_file, platypus_src_path, lm_evaluation_src_path):
     _, env_config = get_program_config(env_name, platypus_src_path)
     finetune_args, finetune_config = get_program_config(fine_tune_name, platypus_src_path)
     output_dir = os.path.join(platypus_src_path, f'experiments/{fine_tune_name}')
     os.makedirs(output_dir, exist_ok=True)
     create_finetune_sh(fine_tune_name, env_config, finetune_args, finetune_config, platypus_src_path)
     create_evaluation_sh(fine_tune_name, env_config, finetune_args, finetune_config, platypus_src_path, lm_evaluation_src_path)
+    create_qa_benchmark(fine_tune_name, env_config, finetune_config, platypus_src_path, qa_test_file)
 
 
 
 def create_finetune_sh(fine_tune_name, env_config, finetune_args, finetune_config, platypus_src_path):
     file_name = os.path.join(platypus_src_path, f"experiments/{fine_tune_name}/finetune_{fine_tune_name}.sh")
+    if 'wandb_run_name' not in finetune_args:
+        finetune_args.append(f'wandb_run_name {fine_tune_name}')
     with open(file_name, 'w', encoding='UTF8') as f:
         f.write(f"set -x\n")
         f.write(f"set -e\n")
@@ -22,6 +26,8 @@ def create_finetune_sh(fine_tune_name, env_config, finetune_args, finetune_confi
         f.write(f'export PYTHONPATH=$PYTHONPATH:{platypus_src_path}\n')
         f.write('\n')
         f.write(f'export CUDA_VISIBLE_DEVICES={env_config["--CUDA_VISIBLE_DEVICES"]}\n')
+        f.write('\n')
+        f.write(f'export WANDB_API_KEY={env_config["--WANDB_API_KEY"]}\n')
         f.write('\n')
         f.write(f'cd {platypus_src_path}\n')
         f.write('\n')
@@ -75,11 +81,37 @@ def create_evaluation_sh(fine_tune_name, env_config, finetune_args, finetune_con
         f.write(f'cd {platypus_src_path}/experiments/{fine_tune_name}\n')
 
 
+def create_qa_benchmark(fine_tune_name, env_config, finetune_config, platypus_src_path, qa_test_file):
+    os.makedirs(env_config['--benchmark_path'], exist_ok=True)
+    file_name = os.path.join(platypus_src_path, f"experiments/{fine_tune_name}/inference_{fine_tune_name}.sh")
+    with open(file_name, 'w', encoding='UTF8') as f:
+        f.write(f"set -x\n")
+        f.write(f"set -e\n")
+        f.write('\n')
+        f.write(f'export PYTHONPATH=$PYTHONPATH:{platypus_src_path}\n')
+        f.write('\n')
+        f.write(f'cd {platypus_src_path}\n')
+        f.write('\n')
+        f.write(f'export CUDA_VISIBLE_DEVICES={env_config["--CUDA_VISIBLE_DEVICES"]}\n')
+        f.write('\n')
+        f.write("python inference.py \\\n")
+        f.write(f"--base_model {finetune_config['--output_dir']}_merged \\\n")
+        f.write(f"--lora_weights null \\\n")
+        f.write(f"--input_csv_path {qa_test_file} \\\n")
+        f.write(f"--output_csv_path {env_config['--result_path']}/{fine_tune_name}_merged/{Path(qa_test_file).stem}.txt \\\n")
+        f.write(f"--output_benchmark_path {env_config['--benchmark_path']} \\\n")
+        f.write(f"--model_name {fine_tune_name}_merged \\\n")
+        f.write(f"--output_qa True \\\n")
+        f.write('\n')
+        f.write(f'cd {platypus_src_path}/experiments/{fine_tune_name}\n')
+
+
 if __name__ == "__main__":
     env_name = sys.argv[1]
     fine_tune_name = sys.argv[2]
+    qa_test_file = sys.argv[3]
     self_path = os.path.abspath(sys.argv[0])
     platypus_src_path = os.path.dirname(os.path.dirname(self_path))
     lm_evaluation_src_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(self_path))), "lm-evaluation-harness")
 
-    setup_experiments(env_name, fine_tune_name, platypus_src_path, lm_evaluation_src_path)
+    setup_experiments(env_name, fine_tune_name, qa_test_file, platypus_src_path, lm_evaluation_src_path)
